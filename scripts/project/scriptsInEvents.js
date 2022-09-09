a18b37bfdcdceb1dd1816c39381aef001888b522
Jerónimo Calandro <72873903+JeronimoCalandro@users.noreply.github.com>
2022-09-08 21:50:14 -0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,21 @@
+
+
+
+const scriptsInEvents = {
+
+		async Egame_Event59_Act1(runtime, localVars)
+		{
+			window.dataLayer = window.dataLayer || [];
+			window.dataLayer.push({'event': 'GameRestart'});
+		},
+
+		async Emenu_Event16_Act1(runtime, localVars)
+		{
+			window.dataLayer = window.dataLayer || [];
+			 window.dataLayer.push({'event': 'GameStart'});
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
